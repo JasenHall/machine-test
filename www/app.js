c0be825afc06f2b99cff13a59d522a7d69a2b3b5
Jasen Hall <jasen@gently.me.uk>
2017-06-11 23:14:58 +0100

    1st code commit

--- www/app.js
+++ www/app.js
@@ -1,5 +1,42 @@
-// Placeholder for Express App
-// 
-// Coming Soon!!!!
-// 
-// Version 2 in a different repository
+var express          = require("express"),
+    mongoose         = require("mongoose"),
+    bodyParser       = require("body-parser"),
+    methodOverride   = require("method-override");
+
+var app = express();
+// ==================================================
+// =      APPLICATION CONFIGURATION SETTINGS        =
+// ==================================================
+// set ejs template as default
+app.set("view engine", "ejs");
+// serve static content from public directory
+app.use(express.static("public"));
+// configure body parser
+app.use(bodyParser.urlencoded({extended: true}));
+// set method override query string parameter
+app.use(methodOverride("_method"));
+// ==================================================
+// =      MONGOOSE CONFIGURATION                    =
+// ==================================================
+// mongoose.connect("mongodb://localhost/test");
+// # database schema
+// var userSchema = new mongoose.Schema({
+//     name: String,
+//     role: String,
+// });
+// var User = mongoose.model("User", userSchema);
+// ==================================================
+// =      ROUTES                                    =
+// ==================================================
+app.get("/", function(req, res){
+    res.render("index");
+});
+app.get("*", function(req, res){
+    res.render("404");
+});
+// ==================================================
+// =      START SERVER LISTENER                     =
+// ==================================================
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server listening on port " + (process.env.PORT || 3000) );
+});
